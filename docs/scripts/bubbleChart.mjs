e848cb287bb36a7519673bef028c9eff12cd6355
Thijs Spijker <thijs.spijker@hva.nl>
2019-11-21 11:57:40 +0100

    add pan boundary

--- docs/scripts/bubbleChart.mjs
+++ docs/scripts/bubbleChart.mjs
@@ -1,6 +1,6 @@
 export function drawCircles(data) {
-	const width = '1000';
-	const height = '1000';
+	const height = 1000;
+	const width = 1000;
 
 	const pack = data => d3.pack()
 		.size([width, height])
@@ -8,13 +8,48 @@ export function drawCircles(data) {
 			.sum(d => Math.sqrt(d.creatorCount)));
 
 	const root = pack(data);
+	const dragSVG = d3.drag()
+		.on("drag", function(d) {
+			// relative x y
+			let dx = d3.event.dx;
+			let dy = d3.event.dy;
+
+			// newXandY
+			let newX = d.dx ? d.dx + dx : d.x + dx;	
+			let newY = d.dy ? d.dy + dy : d.y + dy;
+			
+			let previousMaxX = d.maxX ? d.maxX : 0;
+			let previousMinX = d.minX ? d.minX : 0;
+			let previousMaxY = d.maxY ? d.maxY : 0;
+			let previousMinY = d.minY ? d.minY : 0;
+
+			d.dx = checkMinMax(newX, previousMaxX, previousMinX);
+			d.dy = checkMinMax(newY, previousMaxY, previousMinY);
+
+			
+			// calculate new min/max values for x and y
+			let [minX, maxX] = calculateX(d.r, d.y, d.dy, d.x);
+			let [minY, maxY] = calculateX(d.r, d.x, d.dx, d.y);
+
+			d.minX = minX;
+			d.maxX = maxX;
+			d.minY = minY;
+			d.maxY = maxY;
+
+			// let x = d.dx < 500 ? d.dx : d.dx - 200;
+			// let y = d.dy < 500 ? d.dy : d.dy - 200;
+
+			d3.select(this).attr('viewBox', d => `${d.dx}, ${d.dy}, ${width}, ${height}`);
+		});
 
 	const svg = d3.select('#bubbleChart')
+		.data(root.ancestors())
 		.append('svg')
-		.attr('viewBox', [0, 0, width, height])
+		.attr('viewBox', d => `${d.x}, ${d.y}, ${width}, ${height}`)
 		.attr('font-size', 10)
 		.attr('font-family', 'sans-serif')
-		.attr('text-anchor', 'middle');
+		.attr('text-anchor', 'middle')
+		.call(dragSVG)
 
 	const bubble = svg.selectAll('g')
 		.data(root.leaves())
@@ -22,9 +57,10 @@ export function drawCircles(data) {
 		.append('g')
 		.attr('id', d => d.id)
 		.attr('transform', d => `translate(${d.x},${d.y})`);
-
-
+	
 	console.log(root);
+
+	// console.log(calculateMaxX(500, [500, 250], [500, 500]));
 
 	bubble.append('circle')
 		.attr('class', 'bubbles')
@@ -37,4 +73,36 @@ export function drawCircles(data) {
 		.attr('fill', 'freeze');
 
 	return svg.node();
+}
+
+
+function calculateX(radius, offsetY, y, offsetX) {
+
+	let cy = y - offsetY;
+	let cx = Math.sqrt((Math.pow(radius,2) - Math.pow(cy, 2)));
+
+	let minX = -cx + offsetX;
+	let maxX = cx + offsetX;
+
+	return [minX, maxX];
+
+}
+
+function calculateY(radius, offsetX, x, offsetY) {
+	let cx = x - offsetX;
+	let cy = Math.sqrt((Math.pow(radius,2) - Math.pow(cx, 2)));
+
+	let minY = -cy + offsetY;
+	let maxY = cy + offsetY;
+
+	return [minX, maxX];
+
+}
+
+function checkMinMax(value, max, min){
+
+	if(value <= max && value >= min) return value
+	else if( value < min ) return min
+	else if( value > max ) return max
+
 }
